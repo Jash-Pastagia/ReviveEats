2f1551155490bb445bd95496fc5f3d06fba0230d
PatelDharmik268 <dharmikpatel262008@gmail.com>
2024-03-02 12:51:04 +0530

    dharmik_jenil

--- user/conn.php
+++ user/conn.php
@@ -1,24 +1,26 @@
+
+
 <?php
     $connection = mysqli_connect('localhost','root',''); //connection with server apache (servername,username,pwd)
-    if($connection) 
-    {   
-        echo "connection done !! ";
-    }
-    else
-    {
-        echo "connection is fail !! ";
-    }
+    // if($connection) 
+    // {   
+    //     echo "connection done !! ";
+    // }
+    // else
+    // {
+    //     echo "connection is fail !! ";
+    // }
     echo "<br><br>";
-    $database = mysqli_select_db($connection,'admin');//(connection vari,database_name) select database
-    if($database)
-    {
-        echo "database find !! ";
-    }
-    else
-    {
-        echo "database lost !! ";   
-    }
-    echo "<br><br>";
+    $database = mysqli_select_db($connection,'hackathon');//(connection vari,database_name) select database
+    // if($database)
+    // {
+    //     echo "database find !! ";
+    // }
+    // else
+    // {
+    //     echo "database lost !! ";   
+    // }
+    // echo "<br><br>";
 
 
 ?>
