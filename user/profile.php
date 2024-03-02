f19d100ccd033b3757b29383d4d326cab43bdb71
Jash_Pastagia <jashpastagia0@gmail.com>
2024-03-02 20:25:50 +0530

    Jash user_Done

--- user/profile.php
+++ user/profile.php
@@ -18,7 +18,7 @@ if($_SESSION['name']==''){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Revive Eats</title>
-    <link rel="stylesheet" href="home2.css">
+    <link rel="stylesheet" href="home.css">
     <link rel="stylesheet" href="profile.css">
     <!-- <link rel="stylesheet" href="loginstyle.css"> -->
     <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
@@ -51,6 +51,7 @@ header {
     color: black;
 } */
 .logo {
+    margin-left: 100px;
     font-size: 28px;
     color: var(--navfont);
 }
@@ -103,9 +104,9 @@ header {
             <!-- Profile information section -->
             <p class="headingline">Profile</p>
             <div class="info">
-                <p>Name: <?php echo $_SESSION['name']; ?></p>
-                <p>Email: <?php echo $_SESSION['email']; ?></p>
-                <p>Gender: <?php echo $_SESSION['gender']; ?></p>
+                <p><b>Name: </b>&nbsp;<?php echo $_SESSION['name']; ?></p>
+                <p><b>Email: </b>&nbsp;<?php echo $_SESSION['email']; ?></p>
+                <p><b>Gender: </b>&nbsp;<?php echo $_SESSION['gender']; ?></p>
                 <!-- Additional profile details can be added here -->
                 <a href="logout.php">Logout</a>
             </div>
